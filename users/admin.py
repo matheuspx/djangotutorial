c4adfd99de2d4fd0f6e9bcaa215aa562b74584d0
Matheus <matheus@esolvere.com.br>
2025-09-25 10:39:32 -0300

    commit

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import UserBase
 
-# Register your models here.
+@admin.register(UserBase)
+class CustomUserAdmin(UserAdmin):
+    # Campos exibidos na lista do admin
+    list_display = ("username", "email", "first_name", "last_name", "is_staff", "birth_date")
+    
+    # Campos disponíveis para pesquisa
+    search_fields = ("username", "email", "first_name", "last_name")
+    
+    # Campos filtráveis na barra lateral
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    
+    # Organização dos campos no formulário de edição
+    fieldsets = UserAdmin.fieldsets + (
+        ("Informações adicionais", {"fields": ("bio", "birth_date")}),
+    )
+    
+    # Campos ao criar novo usuário
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Informações adicionais", {"fields": ("bio", "birth_date")}),
+    )
