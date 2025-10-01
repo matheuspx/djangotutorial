839512326f69b5f005b290a1b2de9b0d2775638c
Matheus <matheus@esolvere.com.br>
2025-10-01 11:14:25 -0300

    criando perfil

--- users/models.py
+++ users/models.py
@@ -5,3 +5,16 @@ from django.db import models
 class UserBase(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     birth_date = models.DateField(blank=True, null=True)
+
+from django.conf import settings
+
+class Perfil(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    #foto = models.ImageField(upload_to='fotos_perfil/', blank=True, null=True)
+    descricao = models.TextField(blank=True, null=True)
+    data_nascimento = models.DateField(blank=True, null=True)
+    localizacao = models.CharField(max_length=100, blank=True, null=True)
+    interesses = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
---
+++ users/views_perfil.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView, DeleteView
+from django.db import models
+from django.conf import settings
+
+from users.models import perfil
+from .forms import CustomUserCreationForm
+from django.conf import settings
+import mysql.connector
+
+User = get_user_model()
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host=settings.DATABASES['default']['HOST'],
+        user=settings.DATABASES['default']['USER'],
+        password=settings.DATABASES['default']['PASSWORD'],
+        database=settings.DATABASES['default']['NAME']
+    )
+
+# View de criação de perfil
+def create_perfil(request):
+    if request.method == 'POST':
+        descricao = request.POST.get('descricao')
+        data_nascimento = request.POST.get('data_nascimento')
+        localizacao = request.POST.get('localizacao')
+        interesses = request.POST.get('interesses')
+
+        perfil_instance = perfil.objects.create(
+            user=request.user,
+            descricao=descricao,
+            data_nascimento=data_nascimento,
+            localizacao=localizacao,
+            interesses=interesses
+        )
+        return redirect('user-list')
+    return render(request, 'users/create_perfil.html')
+
+class Perfil(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    descricao = models.TextField(blank=True, null=True)
+    data_nascimento = models.DateField(blank=True, null=True)
+    localizacao = models.CharField(max_length=100, blank=True, null=True)
+    interesses = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
